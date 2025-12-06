c224fad0eff6d0a183dcc2883d319c037e0a9582
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2025-12-07 12:36:07 +1300

    fix a typo

--- debian-12-server-base/run.sh
+++ debian-12-server-base/run.sh
@@ -5,7 +5,7 @@ do
   echo "waiting for activations"
   while [ ! -f /activate ]; do sleep 10; done
   rm /activate
-  echo "activation recieved, waking up"
+  echo "activation received, waking up"
   packer init .
   retVal=$?
   if [ $retVal -ne 0 ]; then
