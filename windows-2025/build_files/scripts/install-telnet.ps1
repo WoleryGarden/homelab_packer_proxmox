d5bae669228105ac347ce0bf3190d4e11fb0a411
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2025-12-07 12:07:15 +1300

    alternate (faster) way to install telnet

--- windows-2025/build_files/scripts/install-telnet.ps1
+++ windows-2025/build_files/scripts/install-telnet.ps1
@@ -1,3 +1,5 @@
 Write-Host "Installing telnet..."
-Install-WindowsFeature -name Telnet-Client
+# Install-WindowsFeature -name Telnet-Client
+dism /online /Enable-Feature /FeatureName:TelnetClient
+# pkgmgr /iu:"TelnetClient"
 "Done installing telnet." | Write-Host
