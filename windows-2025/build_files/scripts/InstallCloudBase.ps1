c16861ca5598045b0b88c9f821e0c6a68e92cac6
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2025-12-07 11:47:03 +1300

    remove progress from install cloudbase so it downloads faster

--- windows-2025/build_files/scripts/InstallCloudBase.ps1
+++ windows-2025/build_files/scripts/InstallCloudBase.ps1
@@ -3,20 +3,21 @@
 Write-Host  "Downloading CloudBase-Init"
 
 try {
-    Write-Host "Downloading CloudBaseInit Software"
-    Invoke-WebRequest -Uri "https://cloudbase.it/downloads/CloudbaseInitSetup_x64.msi" -OutFile CloudbaseInitSetup_x64.msi
+  Write-Host "Downloading CloudBaseInit Software"
+  $ProgressPreference = "SilentlyContinue"
+  Invoke-WebRequest -Uri "https://cloudbase.it/downloads/CloudbaseInitSetup_x64.msi" -OutFile CloudbaseInitSetup_x64.msi
 
 
-    Write-Host "Installing CloudBaseInit"
-    $cloudbase = (Start-Process msiexec.exe -ArgumentList "/i","CloudBaseInitSetup_x64.msi","/qb-","/norestart" -NoNewWindow -Wait -PassThru)
-    if ($cloudbase.ExitCode -ne 0) {
-      Write-Error "Error installing CloudBaseInit Software"
-      exit 1
-    }
-  
-  }
-  catch
-  {
-    Write-Error $_.Exception
+  Write-Host "Installing CloudBaseInit"
+  $cloudbase = (Start-Process msiexec.exe -ArgumentList "/i","CloudBaseInitSetup_x64.msi","/qb-","/norestart" -NoNewWindow -Wait -PassThru)
+  if ($cloudbase.ExitCode -ne 0) {
+    Write-Error "Error installing CloudBaseInit Software"
     exit 1
   }
+  
+}
+catch
+{
+    Write-Error $_.Exception
+    exit 1
+}
